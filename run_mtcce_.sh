9b692e5757fba8507714bae985712be620735115
paiboon.kupthanakorn <kpaiboon@gmail.com>
2020-02-27 13:41:06 +0700

    Add files via upload

--- run_mtcce_.sh
+++ run_mtcce_.sh
@@ -3,4 +3,5 @@
 # with local MQTT
 #python3  a1pyprox.py -I 0.0.0.0  -v -t -M 127.0.0.1 -o canon 40063 54.169.38.75 30240
 
-python3  a1pyprox.py -I 0.0.0.0  -v -t -o canon 40063 54.169.38.75 30240
+# standard
+python3  a1pyprox.py -I 0.0.0.0  -v -t -o canon 40065 54.169.38.75 30240
