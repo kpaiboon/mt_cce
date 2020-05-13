b02b12f636cb4d6fe394b6f260d17f94214928fc
paiboon.kupthanakorn <kpaiboon@gmail.com>
2020-05-13 20:35:36 +0700

    Delete run_mtcce_.sh

--- run_mtcce_.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# with local MQTT
-#python3  c1pyprox.py -I 0.0.0.0  -v -t -M 127.0.0.1 -o canon 40063 54.169.38.75 30240
-
-# standard
-python3  c1pyprox.py -I 0.0.0.0  -v -t -o canon 40065 54.169.38.75 30240
